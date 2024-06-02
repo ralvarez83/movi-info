adf40d3d9c819c8013b264624038cd297c3497f7
Rubén Álvarez González <rubenag83@gmail.com>
2024-06-02 17:06:16 +0200

    Se realiza una prueba del compoennte

--- front-react/test/Context/movies/application/MovieFindById.test.tsx
+++ front-react/test/Context/movies/application/MovieFindById.test.tsx
@@ -1,10 +1,54 @@
-import {expect, test} from '@jest/globals';
+import {expect, test, jest} from '@jest/globals';
+import { act, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { axe, toHaveNoViolations } from "jest-axe";
+import React from 'react';
+import * as react_router from "react-router";
+import {MovieDetails} from '../../../../src/apps/frontend/components/MovieDetails'
+import { DotNetBackRepository } from '../../../../src/Contexts/movies/infraestruture/dotNetBack/DotNetBackRepository';
+import { Movie } from '../../../../src/Contexts/movies/domain/Movie';
+import { VideoMother } from '../domain/VideoMother';
+
+jest.mock("../../../../src/Contexts/movies/infraestruture/dotNetBack/DotNetBackRepository");
+
+expect.extend(toHaveNoViolations);
 
 describe('If i search a movie by a ..', () => {
-  test('existing ID then I receibe info about the movie', () => {
+  test('existing ID then I receibe info about the movie', async () => {
+
+    let moviRepo = new DotNetBackRepository("");
+    const movie: Movie | undefined = VideoMother.Random();
+
+    (moviRepo.findById as jest.Mock).mockResolvedValue(movie as never)
+
+    jest.spyOn(react_router, "useParams").mockReturnValueOnce({ id: movie.id });
     
-  
-    //expect(() => binaryStringToNumber('abc')).toThrow(CustomError);
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+
+    const { container } = render (<MovieDetails repository={moviRepo} />)
+
+    expect(await axe(container)).toHaveNoViolations();
+
+    const title = screen.getByText(movie.title)
+    const imdb = screen.getByLabelText("Link de acceso a la página de IMDB de la película")
+    
+    const overview = screen.getByText(movie.overview)
+
+    expect(title).toBeInTheDocument()
+    expect(imdb.href).toContain(movie.imdbLink)
+    expect(overview).toBeInTheDocument()
   });
   
 });
--- front-react/test/Context/movies/domain/VideoMother.tsx
+++ front-react/test/Context/movies/domain/VideoMother.tsx
@@ -1,24 +1,23 @@
 import {type Movie} from "../../../../src/Contexts/movies/domain/Movie"
-import {jest} from '@jest/globals';
+import crypto from 'crypto';
+
 
 export class VideoMother {
   
   public static Random (): Movie {
     
-    jest.mock("../../../../src/Contexts/movies/domain/Movie")
-
-    const movie : jest.Mocked<Movie>;
-
-    // const movie = {
-    //   adult:;
-    //   verticalImagePath:;
-    //   horizontalImagePath:;
-    //   id: ;
-    //   overview: ;
-    //   title:;
-    //   imdbLink:;
-    //   usersVote:;
-    //  }
+    const randomMovie: Movie = {
+      id: crypto.randomInt(100000).toString(),
+      title: crypto.randomBytes(10).toString('hex'),
+      usersVote: 72,
+      overview: crypto.randomBytes(50).toString('hex'),
+      imdbLink: 'https://www.imdb.com/title/tt' + crypto.randomInt(100000),
+      adult: (crypto.randomInt(100) % 2) === 0,
+      verticalImagePath: crypto.randomBytes(10).toString('hex'),
+      horizontalImagePath: crypto.randomBytes(10).toString('hex')
+    }
+    
+    return randomMovie
   }
 
 }
