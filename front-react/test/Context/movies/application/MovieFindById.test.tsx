09398428ec96f013c41088cfc8a901b5b7598293
Rubén Álvarez González <rubenag83@gmail.com>
2024-06-02 17:41:36 +0200

    Se realiza el test de buscar una pelicula que no existe.

--- front-react/test/Context/movies/application/MovieFindById.test.tsx
+++ front-react/test/Context/movies/application/MovieFindById.test.tsx
@@ -1,5 +1,5 @@
 import {expect, test, jest} from '@jest/globals';
-import { act, render, screen } from "@testing-library/react";
+import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { axe, toHaveNoViolations } from "jest-axe";
 import React from 'react';
@@ -39,6 +39,8 @@ describe('If i search a movie by a ..', () => {
 
     const { container } = render (<MovieDetails repository={moviRepo} />)
 
+    await waitForElementToBeRemoved(() => screen.getByText(/cargando/i))
+
     expect(await axe(container)).toHaveNoViolations();
 
     const title = screen.getByText(movie.title)
@@ -49,6 +51,38 @@ describe('If i search a movie by a ..', () => {
     expect(title).toBeInTheDocument()
     expect(imdb.href).toContain(movie.imdbLink)
     expect(overview).toBeInTheDocument()
+  });
+
+  test('non existing ID then I receibe info about the movie', async () => {
+    let moviRepo = new DotNetBackRepository("");
+    const movie: Movie | undefined = undefined;
+
+    (moviRepo.findById as jest.Mock).mockResolvedValue(movie as never)
+
+    jest.spyOn(react_router, "useParams").mockReturnValueOnce({ id: Math.random().toString() });
+    
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+
+    const { container } = render (<MovieDetails repository={moviRepo} />)
+    await waitForElementToBeRemoved(() => screen.getByText(/cargando/i))
+
+    expect(await axe(container)).toHaveNoViolations();
+
+    const message = screen.getByText(/no encontrada/i)
+
+    expect(message).toBeInTheDocument()
   });
   
 });
