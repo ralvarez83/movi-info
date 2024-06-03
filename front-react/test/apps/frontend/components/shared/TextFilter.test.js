e78319a36b2b299eaa3d0f8bcd0e372ec10e1cfc
Rubén Álvarez González <rubenag83@gmail.com>
2024-06-03 14:22:27 +0200

    Se mejora la descriptibilidad del test

--- front-react/test/apps/frontend/components/shared/TextFilter.test.js
+++ front-react/test/apps/frontend/components/shared/TextFilter.test.js
@@ -4,55 +4,57 @@ import {expect, jest} from '@jest/globals';
 import { TextFilter } from '../../../../../src/apps/frontend/components/shared/TextFilter'
 import { FilterOperator } from '../../../../../src/Contexts/Shared/Domain/Criteria/Filters/FilterTypes.d'
 
-
-describe("Input text to filter", () => {
-  it("calls with more caracteres than minimal (3 by default)", () =>{
-    const setFilter = jest.fn();
-    const placeholder = "busca"
-    const emptyFilter = {
-      field: "textFinder",
-      operator: FilterOperator.CONTAINS,
-      value: ''
-    }
-
-    const expectedFilter = {
-      field: "textFinder",
-      operator: FilterOperator.CONTAINS,
-      value: 'Lor'
-    }
-
-    render (<TextFilter setFilter={setFilter} placeholder={placeholder} filter={emptyFilter} />)
-
-    const filter = screen.getByLabelText(/busca/i);
-    fireEvent.change(filter, {
-      target: { value: expectedFilter.value },
-    });
-
-    expect(setFilter).toHaveBeenCalledWith(expectedFilter);  
+describe ("TextFilter", ()=> {
+  describe("Write more than minimal length (3 by defaul)", () => {
+    it("should call the filter handler", () =>{
+      const setFilter = jest.fn();
+      const placeholder = "busca"
+      const emptyFilter = {
+        field: "textFinder",
+        operator: FilterOperator.CONTAINS,
+        value: ''
+      }
+  
+      const expectedFilter = {
+        field: "textFinder",
+        operator: FilterOperator.CONTAINS,
+        value: 'Lor'
+      }
+  
+      render (<TextFilter setFilter={setFilter} placeholder={placeholder} filter={emptyFilter} />)
+  
+      const filter = screen.getByLabelText(/busca/i);
+      fireEvent.change(filter, {
+        target: { value: expectedFilter.value },
+      });
+  
+      expect(setFilter).toHaveBeenCalledWith(expectedFilter);  
+    })
   })
-
-  it("doesn't call with less caracteres than minimal (3 by default)", () =>{
-    const setFilter = jest.fn();
-    const placeholder = "busca"
-    const emptyFilter = {
-      field: "textFinder",
-      operator: FilterOperator.CONTAINS,
-      value: ''
-    }
-
-    const expectedFilter = {
-      field: "textFinder",
-      operator: FilterOperator.CONTAINS,
-      value: 'Lo'
-    }
-
-    render (<TextFilter setFilter={setFilter} placeholder={placeholder} filter={emptyFilter} />)
-
-    const filter = screen.getByLabelText(/busca/i);
-    fireEvent.change(filter, {
-      target: { value: expectedFilter.value },
-    });
-
-    expect(setFilter).not.toHaveBeenCalled();  
+  describe("Write less than minimal length (3 by defaul)", () => {
+    it("shouldn't call theh filter handler", () =>{
+      const setFilter = jest.fn();
+      const placeholder = "busca"
+      const emptyFilter = {
+        field: "textFinder",
+        operator: FilterOperator.CONTAINS,
+        value: ''
+      }
+  
+      const expectedFilter = {
+        field: "textFinder",
+        operator: FilterOperator.CONTAINS,
+        value: 'Lo'
+      }
+  
+      render (<TextFilter setFilter={setFilter} placeholder={placeholder} filter={emptyFilter} />)
+  
+      const filter = screen.getByLabelText(/busca/i);
+      fireEvent.change(filter, {
+        target: { value: expectedFilter.value },
+      });
+  
+      expect(setFilter).not.toHaveBeenCalled();  
+    })
   })
 })
